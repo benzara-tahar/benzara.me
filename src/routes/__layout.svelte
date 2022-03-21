85aa465792705a7203ad5effb35a8077c9921500
Benzara Tahar Benlahcene <benzara.tahar@gmail.com>
2022-03-21 21:12:24 +0100

    add console logs for intruders

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import { Navbar, Footer } from '$layout';
+	import { logCss as css, randomColor } from '$lib/utils/console.log';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log(
+			'%cHi there intruder 🕵️' +
+				'%cif you spot some 🐞 🐝 🐞 🐜, report that in github issues so that we can make this place cleaner ✨✨\nOtherwise, if you enjoy what you see, ⭐ me on 🐙\nhttps://github.com/benzara-tahar/benzara.me',
+			css(12, 'cyan'),
+			css(
+				12,
+				'white',
+				`background:${randomColor()} ; background: linear-gradient(90deg, ${randomColor()} 0%, ${randomColor()} 49%, ${randomColor()} 100%);`
+			)
+		);
+	});
 </script>
 
 <Navbar />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,36 @@
 <script lang="ts">
 	import { typewriter } from '$lib/transitions/typewriter.transition';
-	import HexagonNeon from '$lib/components/HexagonNeon.svelte';
 </script>
 
-<main class="relative w-full bg-red-400" style="height: calc( 100vh - 100px);">
-	<HexagonNeon className="absolute top-0 left-0 bottom-0 right-0   pointer-events-none" />
-	<div class="flex flex-col mt-10">
+<main class="relative w-full " style="min-height: calc( 100vh - 180px);">
+	<div class="flex flex-col mt-10 p-14 md:p-20 ">
+		<span class="text-lg text-primary-600 dark:text-primary-100 ">
+			Hi, Welcome to my corner on the internet!
+		</span>
 		<h1
-			in:typewriter={{ speed: 100 }}
-			class="text-gray-700 dark:text-gray-200 font-bold text-4xl md:text-5xl text-center tracking-wide relative"
+			class="text-gray-700 dark:text-gray-200 font-bold text-4xl md:text-5xl tracking-wide relative"
 		>
-			Go ahead! build something
-
 			<span
-				class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-purple-400 via-green-400"
+				class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-400 via-yellow-400"
 			>
-				FANCY!
+				Benzara Tahar Benlahcene
 			</span>
-			<span class="rocket"> 🚀 </span>
 		</h1>
+		<h1
+			class="text-gray-600 dark:text-gray-400 font-bold text-4xl md:text-5xl tracking-wide relative"
+		>
+			I am I build things you love.
+		</h1>
+
+		<p
+			class="text-gray-700 dark:text-gray-200 text-xl  max-w-3xl tracking-wider font-light text-justify leading-snug mt-8"
+		>
+			I am a 30 years old [frontend|backend] developer, currently working as an idependent
+			freelancer at Upwork, but Currently, I’m focused on building accessible, performant and
+			beautiful web applications.
+		</p>
 	</div>
 </main>
 
 <style lang="scss">
-	.rocket {
-		position: absolute;
-		animation: jump 0.3s ease-out infinite forwards;
-		cursor: pointer;
-		&:hover {
-			animation-duration: 0.2s;
-		}
-	}
-
-	@keyframes jump {
-		0% {
-			transform: translateY(-2px);
-		}
-
-		50% {
-			transform: translateY(2px);
-		}
-
-		100% {
-			transform: translateY(-2px);
-		}
-	}
 </style>
