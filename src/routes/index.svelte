69fd3606b0237e5fd46403c27ec5e4eb01eb5e4b
Benzara Tahar Benlahcene <benzara.tahar@gmail.com>
2022-05-01 05:30:25 +0100

    feat: add empty project page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -61,20 +61,6 @@
 
 	<!-- experiences -->
 	<ExperiencesSection />
-
-	<!-- Projects -->
-	<div class="flex flex-col my-8 ">
-		<h1
-			class="font-bold leading-tight tracking-tight text-white gradient-text"
-			style="font-size: clamp(1.2rem, 8vw, 3rem);"
-		>
-			Projects
-		</h1>
-		<p class="max-w-md leading-8 tracking-wide text-gray-700 dark:text-gray-200">
-			I have a bunch of project that are not on github, trying to polish them before releasing them
-			to the world, stay tuned, it is comming sooner than you would expect...
-		</p>
-	</div>
 </main>
 
 <style lang="scss">
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main class="container py-8 min-h-screen">
+	<!-- Projects -->
+	<div class="flex flex-col my-8 ">
+		<h1
+			class="font-bold leading-tight tracking-tight text-white gradient-text"
+			style="font-size: clamp(1.2rem, 8vw, 3rem);"
+		>
+			Projects
+		</h1>
+		<p class="max-w-2xl leading-8 tracking-wide text-gray-700 dark:text-gray-200">
+			I have a bunch of project that are not on github, trying to polish them before releasing them
+			to the world, stay tuned, it is comming sooner than you would expect...
+		</p>
+	</div>
+</main>
