2419012f36736926458c5db3c5b8c2e5080c6208
Benzara Tahar Benlahcene <benzara.tahar@gmail.com>
2022-05-05 23:19:10 +0100

    more wip changes

--- src/routes/projects/_data/featured-projects-data.ts
+++ src/routes/projects/_data/featured-projects-data.ts
@@ -6,19 +6,20 @@ export const featuredProjects: Project[] = [
 		shortDescription: 'Visitor Management System',
 		longDescription:
 			'VMS is a solution  that tracks visitors entering your office replacing  the conventional visitor record with a medium that is more accessible. Itgives you access to monitor the visitor that enters your buildings, campuses, or other facilities. It is possible because the visitors have to log-in and use ID badges if they wish to enter certain places so the management can keep an eye on them.',
-		thumbnail: '',
-		url: 'https://something.com',
-		githubUrl: 'https://something.com',
+		thumbnail: '/img/projects/vms.png',
+		url: 'https://lcs-visitor-kiosk.herokuapp.com/',
+		githubUrl: '',
 		tags: [
 			'Asp.net_core',
 			'CQRS',
-			'Mediator',
+			'Mediatr',
 			'XUnit',
 			'QRCoder',
 			'EFCore',
 			'Heroku',
 			'Tailwind',
-			'Angular'
+			'Angular',
+			'NgxTranslate'
 		]
 	},
 	{
@@ -26,20 +27,32 @@ export const featuredProjects: Project[] = [
 		shortDescription: 'School Management System',
 		longDescription: `School management system developed in C# (WPF) that allows to accomplish and automate many tasks such as reports generation, scheduling, manage teachers and students all with an integrated role-based access policy.
 		The application is very flexible, almost everything can be configured starting from school settings (name, year format(trimester/semester)) to the average calculation formulas for each module.`,
-		thumbnail: '',
-		url: 'https://something.com',
-		githubUrl: 'https://something.com',
-		tags: ['CSharp', 'WPF', 'MVVM', 'EFCore', 'SQLite', 'DevExpress', 'InnoSetup']
+		thumbnail: '/img/projects/dzschools.png',
+		url: 'https://dzschools.com',
+		githubUrl: '',
+		tags: [
+			'CSharp',
+			'WPF',
+			'MVVM',
+			'EntityFramework',
+			'SQLite',
+			'DevExpress',
+			'InnoSetup',
+			'CrystalReports'
+		]
 	},
 	{
-		title: 'DZSCHOOLS',
-		shortDescription: 'School Management System',
-		longDescription: `School management system developed in C# (WPF) that allows to accomplish and automate many tasks such as reports generation, scheduling, manage teachers and students all with an integrated role-based access policy.
-		The application is very flexible, almost everything can be configured starting from school settings (name, year format(trimester/semester)) to the average calculation formulas for each module.`,
-		thumbnail: '',
-		url: 'https://something.com',
-		githubUrl: 'https://something.com',
-		tags: ['CSharp', 'WPF', 'MVVM', 'EFCore', 'SQLite', 'DevExpress', 'InnoSetup']
+		title: 'Corroboration',
+		shortDescription: 'Document Sharing application for cnr.dz',
+		longDescription: `The application offers a possibility corroborate between the 51 CNR agencies located in the states all around the country. The purpose is to verify and validate the insurer career periods in other states because for now, a decentralized solution is used.
+		You can think of it as an emailing service but with business constraint and workflow validation.
+		The application offers detailed insight of the number of requests sent and received by each agency and their status (pending, resolved, ...etc.). This allow the administration to take the proper actions and decisions.
+		More the 50K request have been sent via the application. in which more then 100K attachement that occupies arround 61Go have been uploaded to the server.
+		The web application is for internal use by the CNR agencies.`,
+		thumbnail: 'img/projects/corroboration.png',
+		url: '',
+		githubUrl: '',
+		tags: ['CSharp', 'AngularJS', 'Asp.net_core', 'SqlServer', 'EFCore']
 	}
 	// {
 	// 	title: '',
